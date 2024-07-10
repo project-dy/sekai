c81540cabb0cbbfa4b90be98e53072578caa8e86
project-dy <kero122899@gmail.com>
2024-07-10 15:59:55 +0900

    refactor: Update file paths and server configuration

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ dotenv.config();
 const express = require("express");
 const app = express();
 
-app.get("/auth", (req, res) => {
+/*app.get("/auth", (req, res) => {
   res.json({
     c: 500,
     m: "Not implemented",
   });
-});
+});*/
+
+app.use(express.static('public'))
 
 app.listen(process.env.PORT, () => {
-  console.log('Server is running on port http://localhost' + process.env.PORT);
+  console.log('Server is running on port http://localhost:' + process.env.PORT);
 });
