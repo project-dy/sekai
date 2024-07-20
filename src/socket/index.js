e4ad9b306ae4ddf6024abb15e3dd2d4316693efc
project-dy <kero122899@gmail.com>
2024-07-20 13:05:53 +0900

    Console shows the image

--- src/socket/index.js
+++ src/socket/index.js
@@ -9,7 +9,7 @@ http 하이퍼       텍스트 트랜스퍼    프로토콜
 
 
      선언 상수 변수 함수 객체 리스트 튜플
-     사용 
+     사용
      조건문
      반복문
      함수
@@ -37,46 +37,45 @@ function f(x) {
 
 f(3); // = 9
 
-const 결과 = f(3); 
+const 결과 = f(3);
 console.log(결과); // = 9
 
 */
 
+const WebSocket = require("ws"); // 웹소켓 모듈 불러오기
+const fs = require("fs"); // fs 모듈 불러오기: file system 파일 관리를 위한 모듈
+const path = require("path"); // path 모듈 불러오기 경로
+const rootPath = path.resolve(__dirname, "../../"); // public 폴더의 절대경로를 publicPath에 저장
+const dataPath = path.resolve(rootPath, "data"); // data 폴더의 절대경로를 dataPath에 저장
 
-
-const WebSocket = require('ws'); // 웹소켓 모듈 불러오기
-const fs = require('fs'); // fs 모듈 불러오기: file system 파일 관리를 위한 모듈
-const path = require('path'); // path 모듈 불러오기 경로
-const rootPath = path.resolve(__dirname, '../../'); // public 폴더의 절대경로를 publicPath에 저장
-const dataPath = path.resolve(rootPath, 'data'); // data 폴더의 절대경로를 dataPath에 저장
-
-const Sekai = require('../../utils/index'); // utils 모듈 로드
+const Sekai = require("../../utils/index"); // utils 모듈 로드
 const sekai = new Sekai(); // Sekai 클래스 인스턴스 생성
 //sekai.greet(); // Hello, Sekai 출력
 
-function webSocketServer( server ) {
+function webSocketServer(server) {
   // Create a WebSocket server
   const wss = new WebSocket.Server({ server });
-  
+
   // Store connected clients
   const clients = [];
   // Handle incoming connections
-  wss.on('connection', (ws, req) => {
+  wss.on("connection", (ws, req) => {
     // Get the "rn" parameter from the request URL
     // const urlParams = new URLSearchParams(req.url);
     // const rn = urlParams.get('rn');
-    const rn = req.url.split('?rn=')[1];
+    //const rn = req.url.split('?rn=')[1];
     // console.log(rn);
 
     //
 
     // Handle incoming messages
-    ws.on('message', (message) => {
-      
+    ws.on("message", (message) => {
+      const parsed = JSON.parse(message);
+      console.log(parsed);
     });
 
     // Handle connection close
-    ws.on('close', () => {
+    ws.on("close", () => {
       // Remove the WebSocket connection from the stored clients
       delete clients[rn];
       console.log(`Client ${rn} disconnected`);
