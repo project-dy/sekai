dd059bf81f67c3f76c9ad3d82582bf0165d80e32
project-dy <kero122899@gmail.com>
2024-08-08 20:07:48 +0900

    Hello sveltekit

---
+++ src/routes/+page.ts
@@ -0,0 +1,12 @@
+// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in production
+export const prerender = true;
+
+import "./app.css";
+/*import App from "./App.svelte";
+
+const app = new App({
+  target: document.getElementById("app")!,
+});
+
+export default app;*/
