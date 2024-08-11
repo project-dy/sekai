e67d283115b895fda4565ac28fd336c9d6462775
project-dy <kero122899@gmail.com>
2024-08-11 11:42:10 +0900

    Server kudasai

---
+++ server/index.ts
@@ -0,0 +1,113 @@
+/*let store = {
+	user: [],
+	music: [],
+};
+
+let template = {
+	user: {
+		name: "",
+		sn: "",
+		order: "", // 입장 순서
+		solved: "", // 푼 문제
+		correct: [], // 정답 여부 배열
+		score: 0, // 점수
+	},
+	music: {
+		title: "",
+		artist: "",
+		source: "", // is Downloaded(local) or from http
+		url: "",
+		answer: "",
+		hint: "",
+		score: 0,
+	},
+};
+*/
+//const dotenv = require("dotenv");
+import dotenv from "dotenv";
+dotenv.config();
+
+//const express = require("express");
+import express from "express";
+const app = express();
+
+// nunjucks 설정
+/*const nunjucks = require("nunjucks");
+nunjucks.configure("public", {
+	express: app,
+});
+app.set("view engine", "html");
+app.engine("html", nunjucks.render);*/
+
+/*app.get("/auth", (req, res) => {
+  res.json({
+    c: 500,
+    m: "Not implemented",
+  });
+});*/
+
+//app.use(express.static("public"));
+app.use("/node_modules", express.static("node_modules"));
+/*
+// 기본적으로 nunjucks를 이용한 redering을 모든 get요청에 대하여 처리
+app.get("/", (req, res) => {
+	res.render("index");
+});*/
+
+//const webSocketServer = require("./socket/index"); // socket 모듈 로드
+import webSocketServer from "./socket/index";
+
+/*const server = app.listen(process.env.PORT, () => {
+	console.log("Server is running on port http://localhost:" + process.env.PORT);
+});*/
+import http from "http";
+const server = http.createServer(app);
+server.listen(process.env.PORT || 3000, () => {
+  console.log(
+    "Server is running on port http://localhost:" + process.env.PORT || 3000,
+  );
+});
+
+/*import ViteExpress from "vite-express";
+ViteExpress.bind(app, server);*/
+/*const server = ViteExpress.listen(app, Number(process.env.PORT) || 3000, () => {
+	console.log(
+		"Server is running on port http://localhost:" + process.env.PORT || 3000,
+	);
+});*/
+//const Sekai = require("../../utils/index"); // utils 모듈 로드
+import Sekai from "../utils/index";
+const sekai = new Sekai(); // Sekai 클래스 인스턴스 생성
+sekai.greet(); // Hello, Sekai 출력
+//webSocketServer(server); // socket 서버 실행
+
+// vite dev server
+/*import { createServer as createViteServer } from "vite";
+async function createVite() {
+	const vite = await createViteServer();
+	await vite.listen();
+	const server = vite.httpServer;
+}
+createVite();*/
+webSocketServer(server);
+
+app.get("*", express.static("dist")); // vite build 결과물을 제공
+
+app.get("/b/api", (req, res) => {
+  res.json({
+    c: 501,
+    m: "Not implemented",
+  });
+});
+
+//const util = new sekai.Util();
+//const util = sekai.getUtil();
+//util.crawl();
+
+// /api/list 접속자 리스트
+app.get("/b/api/list", (req, res) => {
+  res.json({
+    c: 501,
+    m: "Not implemented",
+  });
+});
---
+++ utils/index.ts
@@ -0,0 +1,521 @@
+//const fs = require("fs");
+//const path = require("path");
+//const axios = require("axios");
+//const cheerio = require("cheerio");
+
+import fs from "fs";
+import path from "path";
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+//const search = require("youtube-search");
+//const ytdl = require("@distube/ytdl-core");
+//const dotenv = require("dotenv");
+import search from "youtube-search";
+import ytdl from "@distube/ytdl-core";
+import dotenv from "dotenv";
+//dotenv.config();
+import { fileURLToPath } from "url";
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const key = process.env.YOUTUBE_API_KEY;
+
+const opts = {
+  maxResults: 1,
+  key: key,
+};
+
+interface Params {
+  command: string;
+  params: { string: string }[];
+  flags: string[];
+}
+
+class Admin {
+  constructor() {
+    //this.name = "Admin";
+  }
+
+  room(params: Params) {
+    console.log("room");
+    return {
+      c: 501,
+      m: "Not implemented",
+    };
+    if (params.flags[0] == "add") {
+      console.log("add");
+      return {
+        c: 200,
+        m: "Success",
+      };
+    }
+  }
+
+  async crawl(params: Params) {
+    const util = new Util();
+    // Util.crawl();
+    //const result = Util.crawl(params);
+    //console.log(result);
+    //return result;
+    const rootPath = path.resolve(__dirname, "../");
+    const dataPath = path.resolve(rootPath, "data");
+    // If there is no data folder, create one
+    if (!fs.existsSync(dataPath)) {
+      fs.mkdirSync(dataPath);
+    }
+    // If there is no crawled folder, create one
+    if (!fs.existsSync(path.resolve(dataPath, "crawled"))) {
+      fs.mkdirSync(path.resolve(dataPath, "crawled"));
+    }
+
+    //console.log(JSON.stringify({ data: params }));
+    if (params.params[0]?.string === "iChart") {
+      const result = await util.crawliChart();
+      fs.writeFileSync(
+        path.resolve(dataPath, "crawled/iChart.json"),
+        JSON.stringify({ data: result }),
+        //String(result),
+        { encoding: "utf8", flag: "w" },
+      );
+      //return true;
+      return JSON.stringify({ data: result });
+    }
+    const result = await util.crawlMelon();
+    fs.writeFileSync(
+      path.resolve(dataPath, "crawled/melon.json"),
+      JSON.stringify({ data: result }),
+      //String(result),
+      { encoding: "utf8", flag: "w" },
+    );
+    //return true;
+    return JSON.stringify({ data: result });
+  }
+
+  async download(params: Params) {
+    const util = new Util();
+    // Util.downloadAudio();
+    const chartSource = params.params[0]?.string;
+    const until = Number(params.params[1]?.string);
+    const chartFile = path.resolve(
+      __dirname,
+      `../data/crawled/${chartSource}.json`,
+    );
+    const outputDir = path.resolve(__dirname, "../data/audio");
+    return util.downloadAllAudio(chartFile, outputDir, until);
+  }
+
+  async doIt(params: Params): Promise<string> {
+    const command = params.command;
+    switch (command) {
+      case "crawl": {
+        const res = await this.crawl(params);
+        if (res) {
+          return JSON.stringify({ c: 200, m: String(res) });
+        }
+        break;
+      }
+      case "download": {
+        const res = await this.download(params);
+        if (res) {
+          return JSON.stringify({ c: 200, m: String(res) });
+        }
+        break;
+      }
+      default:
+        return "Command Not Found";
+    }
+    return "Command Not Found";
+  }
+}
+
+class Normal {}
+
+class Sekai {
+  name: string;
+  Util: typeof Util;
+  Admin: typeof Admin;
+
+  constructor() {
+    this.name = "Sekai";
+    this.Util = Util;
+    this.Admin = Admin;
+    /*this.Util = new Util();
+		this.Admin = new Admin();*/
+  }
+
+  greet() {
+    console.log(`Hello, ${this.name}`);
+  }
+
+  register() {
+    console.log("register");
+    return {
+      c: 501,
+      m: "Not implemented",
+    };
+  }
+
+  getUtil() {
+    return new Util();
+  }
+
+  getAdmin() {
+    return new Admin();
+  }
+}
+
+interface Song {
+  rank: string;
+  albumArt: string | undefined;
+  title: string;
+  artist: string;
+  album: string;
+}
+
+interface MelonSong {
+  rank: string;
+  albumArt: string | undefined;
+  title: string;
+  artist: string;
+  album: string;
+}
+
+class Util {
+  name: string;
+  constructor() {
+    this.name = "Util";
+  }
+
+  async downloadAllAudio(
+    chartFile: string,
+    folderPath: string,
+    until = 100,
+  ): Promise<string[]> {
+    const chart = JSON.parse(fs.readFileSync(chartFile, "utf-8")).data.slice(
+      0,
+      until,
+    );
+    const audioList: string[] = [];
+    for (const song of chart) {
+      console.log(song);
+      const rank = song.rank;
+      const name = song.title;
+      const artist = song.artist;
+      const audioPath = await this.downloadAudio(
+        rank,
+        name,
+        artist,
+        folderPath,
+      );
+      audioList.push(audioPath);
+    }
+    /*const song = chart[0];
+		console.log(song);
+		const rank = song.rank;
+		const name = song.title;
+		const artist = song.artist;
+		const audioPath = await this.downloadAudio(rank, name, artist, folderPath);
+		audioList.push(audioPath);*/
+    return audioList;
+  }
+
+  async downloadAudio(
+    rank: string,
+    name: string,
+    artist: string,
+    audioPath: string,
+  ): Promise<string> {
+    const searchValue = `${name} ${artist} lyrics`;
+    const searchResult = (await search(searchValue, opts)).results;
+    console.log(searchResult);
+    const videoId = searchResult[0].link;
+    //const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    //const info = await ytdl.getInfo(videoUrl);
+    //const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+    // Get the date of now ex.20241231
+    /*const date = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+    const audioPath = path.resolve(__dirname, `../data/audio/${date}`);*/
+    // If there is no audio folder, create one
+    if (!fs.existsSync(path.resolve(__dirname, "../data/audio"))) {
+      fs.mkdirSync(path.resolve(__dirname, "../data/audio"));
+    }
+    // If there is no date folder, create one
+    if (!fs.existsSync(audioPath)) {
+      fs.mkdirSync(audioPath);
+    }
+    /*ytdl.chooseFormat(info.formats, { quality: "highestaudio" }).pipe(
+      fs.createWriteStream(
+        path.resolve(audioPath, `${name}-${artist}.mp3`),
+      ),
+    );*/
+    //console.log(videoId);
+    /*ytdl(videoId).pipe(
+			//ytdl(videoId, { quality: "highestaudio" }).pipe(
+			fs.createWriteStream(
+				path.resolve(audioPath, `${rank}-${name}-${artist}.mp4`),
+			),
+		);*/
+    const info = await ytdl.getInfo(videoId);
+    const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+    console.log(audioFormats);
+    //const audio = audioFormats.find((format) => format.container === "m4a");
+    const audio = audioFormats.find((format) => format.container === "mp4");
+    if (!audio) {
+      throw new Error("No audio format found");
+      //return;
+    }
+    const audioStream = ytdl.downloadFromInfo(info, {
+      format: audio,
+    });
+    const audioFile = fs.createWriteStream(
+      path.resolve(audioPath, `${name}-${artist}.mp4`),
+    );
+    audioStream.pipe(audioFile);
+    /*await new Promise((resolve, reject) => {
+			ytdl(videoId, { quality: "highestaudio" })
+				.pipe(
+					fs.createWriteStream(
+						path.resolve(audioPath, `${name}-${artist}.mp4`),
+					),
+				)
+				.on("finish", resolve)
+				.on("error", reject);
+		});*/
+
+    return `${name}-${artist}.mp4`;
+  }
+
+  async crawlMelon() {
+    const list: MelonSong[] = [];
+    const melonURL = "https://www.melon.com/chart/index.htm";
+    // Connect to the website and get tbody
+    const html = await axios.get(melonURL);
+    const $ = cheerio.load(html.data);
+    const trList = $("tbody>tr");
+    //console.log(trList);
+    trList.each((index, element) => {
+      const rank = $(element).find("td:nth-child(2)>div>span").text().trim();
+      const albumArt = $(element)
+        .find("td:nth-child(4)>div>a>img")
+        .attr("src")
+        ?.replace("/120", "/480");
+      const title = $(element)
+        .find("td:nth-child(6)>div>div>div:nth-child(1)>span>a")
+        .text()
+        .trim();
+      const artist = $(element)
+        .find("td:nth-child(6)>div>div>div:nth-child(3)>a")
+        .text()
+        .trim();
+      const album = $(element)
+        .find("td:nth-child(7)>div>div>div>a")
+        .text()
+        .trim();
+      /*console.log(
+        `rank: ${rank}, albumArt: ${albumArt}, title: ${title}, artist: ${artist}, album: ${album}`,
+      );*/
+      list.push({
+        rank: rank,
+        albumArt: albumArt,
+        title: title,
+        artist: artist,
+        album: album,
+      });
+    });
+    return list;
+  }
+
+  async crawliChart() {
+    const iChartURL = "https://www.instiz.net/iframe_ichart_score.htm";
+    // Connect to the website and get tbody
+    const html = await axios.get(iChartURL);
+    const $ = cheerio.load(html.data);
+    /*
+    root: .spage_intistore_body,
+    first-i {
+      root: .spage_score_item_1st,
+      albumArt: root>.ichart_score_img>div>img,
+      song: root>.ichart_score_song>div:nth-child(1)>b,
+      album: root>.ichart_score_song>div:nth-child(2)>span>a,
+      rank: 1,
+    },
+    first-b {
+      root: .spage_score_bottom,
+      youtube: root>div:nth-child(1)>.scoreinfo_top>.inner>.ichart_submenu>ul>li:nth-child(1)>a, // "https://www.youtube.com/watch?v="+href.split(",'")[0].split("')")[0]
+      jaksa: root>div:nth-child(1)>.scoreinfo_top>.inner>.ichart_submenu>ul>li:nth-child(3)>span:nth-child(2), // onclick.split("('")[1].split("')")[0]
+      jakgok: root>div:nth-child(1)>.scoreinfo_top>.inner>.ichart_submenu>ul>li:nth-child(3)>span:nth-child(4), // onclick.split("('")[1].split("')")[0]
+      pyeongok: root>div:nth-child(1)>.scoreinfo_top>.inner>.ichart_submenu>ul>li:nth-child(3)>span:nth-child(6), // onclick.split("('")[1].split("')")[0]
+
+    },
+    other-i {
+      root: .spage_score_item, // in spage_intistore_body. with nth-child 2nd is 7th, 3rd is 8th...
+      rank: .ichart_score2_rank,
+      albumArt: .ichart_score2_ment>div>img,
+      song: .ichart_score2_song1,
+      album: .ichart_score2_song2>span>a,
+      artist: .ichart_score2_artist,
+
+    },
+    other-b {
+      root: .ichart_submenu>ul, // in ichart_submenu. with nth-child 2nd is 8th, 3rd is 9th...
+      youtube: .ichart_mv>a, // "https://www.youtube.com/watch?v="+href.split(",'")[0].split("')")[0]
+      jaksa: .showinfo>span:nth-child(2), // onclick.split("('")[1].split("')")[0]
+      jakgok: .showinfo>span:nth-child(4), // onclick.split("('")[1].split("')")[0]
+      pyeongok: .showinfo>span:nth-child(6), // onclick.split("('")[1].split("')")[0]
+    }
+
+    */
+
+    let list = [];
+
+    const first = $(".spage_score_item_1st");
+    const firstAlbumArt = first.find(".ichart_score_img>div>img").attr("src");
+    const firstSong = first
+      .find(".ichart_score_song>div:nth-child(1)>b")
+      .text();
+    const firstAlbum = first
+      .find(".ichart_score_song>div:nth-child(2)>span>a")
+      .text();
+    const firstRank = 1;
+    const firstArtist = first
+      .find(".ichart_score_artist>div:nth-child(1)>b")
+      .text();
+    const firstB = $(".spage_score_bottom");
+    const firstJaksa = firstB
+      .find(
+        ".scoreinfo_top>.inner>.ichart_submenu>ul>li:nth-child(3)>span:nth-child(2)",
+      )
+      .attr("onclick")
+      ?.split("('")[1]
+      .split("')")[0];
+    const firstJakgok = firstB
+      .find(
+        ".scoreinfo_top>.inner>.ichart_submenu>ul>li:nth-child(3)>span:nth-child(4)",
+      )
+      .attr("onclick")
+      ?.split("('")[1]
+      .split("')")[0];
+    const firstPyeongok = firstB
+      .find(
+        ".scoreinfo_top>.inner>.ichart_submenu>ul>li:nth-child(3)>span:nth-child(6)",
+      )
+      .attr("onclick")
+      ?.split("('")[1]
+      .split("')")[0];
+
+    const firstYoutube = firstB
+      .find(".scoreinfo_top>.inner>.ichart_submenu>ul>li:nth-child(1)>a")
+      .attr("href")
+      ?.split(",'")[0]
+      .split("')")[0];
+
+    /*console.log(
+      `rank: ${firstRank}, albumArt: ${firstAlbumArt}, song: ${firstSong}, album: ${firstAlbum}, Jaksa: ${firstJaksa}, Jakgok: ${firstJakgok}, Pyeongok: ${firstPyeongok}`,
+    );*/
+
+    list.push({
+      rank: firstRank,
+      albumArt: "https://" + firstAlbumArt,
+      song: firstSong,
+      album: firstAlbum,
+      artist: firstArtist,
+      jaksa: firstJaksa,
+      jakgok: firstJakgok,
+      pyeonkok: firstPyeongok,
+      youtube: "https://www.youtube.com/watch?v=" + firstYoutube,
+    });
+
+    //console.log(list[0]);
+
+    const other = $(".spage_score_item");
+    other.each((index, element) => {
+      const rank = $(element).find(".ichart_score2_rank").text().trim();
+      const albumArt = $(element)
+        .find(".ichart_score2_ment>div>img")
+        .attr("src");
+      const song = $(element).find(".ichart_score2_song1").text().trim();
+      const album = $(element)
+        .find(".ichart_score2_song2>span>a")
+        .text()
+        .trim();
+      const artist = $(element).find(".ichart_score2_artist1").text().trim();
+      const jaksa = $(element)
+        .next()
+        .find(".showinfo>span:nth-child(2)")
+        .attr("onclick")
+        ?.split("('")[1]
+        ?.split("')")[0];
+      const jakgok = $(element)
+        .next()
+        .find(".showinfo>span:nth-child(4)")
+        .attr("onclick")
+        ?.split("('")[1]
+        ?.split("')")[0];
+      const pyeongok = $(element)
+        .next()
+        .find(".showinfo>span:nth-child(6)")
+        .attr("onclick")
+        ?.split("('")[1]
+        ?.split("')")[0];
+      const youtube = $(element)
+        .next()
+        .find(".ichart_mv>a")
+        .attr("href")
+        ?.split(",'")[1]
+        ?.split("')")[0];
+      /*console.log(
+        `rank: ${rank}, albumArt: ${albumArt}, song: ${song}, album: ${album}, jaksa: ${jaksa}, jakgok: ${jakgok}, pyeongok: ${pyeongok}, youtube: ${youtube}`,
+      );*/
+      list.push({
+        rank: rank,
+        albumArt: "https://" + albumArt,
+        song: song,
+        album: album,
+        artist: artist,
+        jaksa: jaksa,
+        jakgok: jakgok,
+        pyeonkok: pyeongok,
+        youtube: "https://www.youtube.com/watch?v=" + youtube,
+      });
+    });
+
+    console.log(list);
+    return list;
+  }
+
+  /*async crawlMelon() {
+    const melonURL = "https://www.melon.com/chart/index.htm";
+    // Connect to the website and get tbody
+    const html = await axios.get(melonURL);
+    const $ = cheerio.load(html.data);
+    const trList = $("tbody>tr");
+    //console.log(trList);
+    trList.each((index, element) => {
+      const rank = $(element).find("td:nth-child(2)>div>span").text().trim();
+      const albumArt = $(element)
+        .find("td:nth-child(4)>div>a>img")
+        .attr("src")
+        .replace("/120", "/480");
+      const title = $(element)
+        .find("td:nth-child(6)>div>div>div:nth-child(1)>span>a")
+        .text()
+        .trim();
+      const artist = $(element)
+        .find("td:nth-child(6)>div>div>div:nth-child(3)>a")
+        .text()
+        .trim();
+      const album = $(element)
+        .find("td:nth-child(7)>div>div>div>a")
+        .text()
+        .trim();
+      console.log(
+        `rank: ${rank}, albumArt: ${albumArt}, title: ${title}, artist: ${artist}, album: ${album}`,
+      );
+    });
+  }*/
+}
+
+//module.exports = Sekai;
+export default Sekai;
