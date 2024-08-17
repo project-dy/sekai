091bae46038147fd1247dff2a6ed8c0594e45867
project-dy <kero122899@gmail.com>
2024-08-17 20:41:01 +0900

    Bad Sveltejs adapter-static but i made it better lol

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	server: {
+		host: "0.0.0.0",
 		hmr: {
 			clientPort: Number(process.env.PORT) + 1 || 3000 + 1,
 			port: Number(process.env.PORT) + 1 || 3000 + 1
